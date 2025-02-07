94e4da12f1b7026b6d88707dbeeb886bd4d5bf0a
Jakub Suliga <j.p.suliga@gmail.com>
2025-02-07 07:07:05 +0100

    Add Street class to represent road attributes and manage vehicles

---
+++ src/simulation/street.py
@@ -0,0 +1,7 @@
+class Street:
+    def __init__(self, length, speed_limit, start, end):
+        self.length = length
+        self.speed_limit = speed_limit
+        self.start = start
+        self.end = end
+        self.vehicles = []
