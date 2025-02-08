62a5cba8fdcbe92b031168eed7156ae8cee458a1
Jakub Suliga <j.p.suliga@gmail.com>
2025-02-08 06:59:26 +0100

    Refactor Intersection class to consolidate street handling and enhance simulate method for vehicle movement

--- src/simulation/intersection.py
+++ src/simulation/intersection.py
@@ -3,9 +3,17 @@ from vehicle import Vehicle
 
 
 class Intersection:
-    def __init__(self, incoming_streets, outgoing_streets):
-        self.incoming_streets = incoming_streets
-        self.outgoing_streets = outgoing_streets
+    def __init__(self, streets):
+        self.streets = streets
 
-    def simulate(self):
-        self.incoming_streets[0].getLastVehicle().get_destination()
+    def simulate(self, street: int):
+        dest = self.streets[street].getLastVehicle().get_destination()
+        if dest == 0:
+            vehc = self.streets[street].removeVehicle()
+            self.streets[(street + 1) % 3].addVehicle(vehc)
+        elif dest == 1:
+            vehc = self.streets[street].removeVehicle()
+            self.streets[(street + 3) % 3].addVehicle(vehc)
+        else:
+            vehc = self.streets[street].removeVehicle()
+            self.streets[(street + 2) % 3].addVehicle(vehc)
