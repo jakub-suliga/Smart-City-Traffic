b5c95663b4b770a894301e2249c60f41df083986
Jakub Suliga <j.p.suliga@gmail.com>
2025-02-08 06:52:46 +0100

    Implement simulation logic in Intersection class to retrieve destination of the last vehicle

--- src/simulation/intersection.py
+++ src/simulation/intersection.py
@@ -1,7 +1,11 @@
+from street import Street
+from vehicle import Vehicle
+
+
 class Intersection:
     def __init__(self, incoming_streets, outgoing_streets):
         self.incoming_streets = incoming_streets
         self.outgoing_streets = outgoing_streets
 
     def simulate(self):
-        pass
+        self.incoming_streets[0].getLastVehicle().get_destination()
