821267d492cc3619a6aaa9722e4ba312085587ae
Jakub Suliga <j.p.suliga@gmail.com>
2025-02-08 06:46:43 +0100

    Add simulate method to Intersection class for future simulation implementation

--- src/simulation/intersection.py
+++ src/simulation/intersection.py
@@ -2,3 +2,6 @@ class Intersection:
     def __init__(self, incoming_streets, outgoing_streets):
         self.incoming_streets = incoming_streets
         self.outgoing_streets = outgoing_streets
+
+    def simulate(self):
+        pass
